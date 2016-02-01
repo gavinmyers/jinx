2c606ea7f1503fef5f7f0f8d64ed9d5137c854a2
gavin myers <gavin@q.local>
2016-01-31 21:44:25 -0600

    box2d

--- core/src/com/jinx/Jinx.java
+++ core/src/com/jinx/Jinx.java
@@ -2,88 +2,163 @@ package com.jinx;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 
-public class Jinx extends ApplicationAdapter {
+public class Jinx extends ApplicationAdapter implements InputProcessor {
     SpriteBatch batch;
-    Sprite sprite;
-    Texture img;
     World world;
+    final float PIXELS_TO_METERS = 100f;
+    Texture img;
+    TextureRegion[] monsters = new TextureRegion[4];
+    Sprite m1;
+    Sprite m2; 
     Body body;
-
+    Body body2;
+    
     @Override
     public void create() {
 
+        
         batch = new SpriteBatch();
-        // We will use the default LibGdx logo for this example, but we need a 
-        img = new Texture("badlogic.jpg");
-        sprite = new Sprite(img);
+        world = new World(new Vector2(0, -100f), true);
+        img = new Texture("Humanoid0.png");
 
-        // Center the sprite in the top/middle of the screen
-        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
-                Gdx.graphics.getHeight() / 2);
-
-        // Create a physics world, the heart of the simulation.  The Vector 
-        world = new World(new Vector2(0, -10f), true);
-
-        // Now create a BodyDefinition.  This defines the physics objects type 
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        // We are going to use 1 to 1 dimensions.  Meaning 1 in physics engine 
-        // Set our body to the same position as our sprite
-        bodyDef.position.set(sprite.getX(), sprite.getY());
-
-        // Create a body in the world using our definition
-        body = world.createBody(bodyDef);
-
-        // Now define the dimensions of the physics shape
-        PolygonShape shape = new PolygonShape();
-        // We are a box, so this makes sense, no?
-        // Basically set the physics polygon to a box with the same dimensions 
-        shape.setAsBox(sprite.getWidth()/2, sprite.getHeight()/2);
-
-        // FixtureDef is a confusing expression for physical properties
-        // Basically this is where you, in addition to defining the shape of the 
-        // you also define it's properties like density, restitution and others 
-        // If you are wondering, density and area are used to calculate over all 
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = shape;
-        fixtureDef.density = 1f;
-
-        Fixture fixture = body.createFixture(fixtureDef);
-
-        // Shape is the only disposable of the lot, so get rid of it
-        shape.dispose();
+        
+        {
+            m1 = new Sprite(img, 0, 0, 16, 16);
+            m1.setPosition(100, 100);        	
+	        BodyDef bodyDef = new BodyDef();
+	        bodyDef.type = BodyDef.BodyType.DynamicBody;
+	        bodyDef.position.set(m1.getX(), m1.getY());
+	        body = world.createBody(bodyDef);        
+	        PolygonShape shape = new PolygonShape();
+	        shape.setAsBox(m1.getWidth()/2, m1.getHeight()/2);
+	        FixtureDef fixtureDef = new FixtureDef();
+	        fixtureDef.shape = shape;
+	        fixtureDef.density = 1f;
+	        Fixture fixture = body.createFixture(fixtureDef);
+	        shape.dispose();
+        }
+        
+        
+        {
+        	m2 = new Sprite(img, 0, 16, 16, 16);
+        	m2.setPosition(100, 25);
+            BodyDef bodyDef = new BodyDef();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set(m2.getX(), m2.getY());
+            body2 = world.createBody(bodyDef);        
+            PolygonShape shape = new PolygonShape();
+            shape.setAsBox(m2.getWidth()/2, m2.getHeight()/2);
+            FixtureDef fixtureDef = new FixtureDef();
+            fixtureDef.shape = shape;
+            fixtureDef.density = 1f;
+            Fixture fixture = body2.createFixture(fixtureDef);
+            shape.dispose();        
+        }
+        
+        
+        
     }
+
 
     @Override
     public void render() {
-
-        // Advance the world, by the amount of time that has elapsed since the 
-        // Generally in a real game, dont do this in the render loop, as you are 
-        // update rate to the frame rate, and vice versa
-        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
-
-        // Now update the spritee position accordingly to it's now updated 
-        sprite.setPosition(body.getPosition().x, body.getPosition().y);
-
-        // You know the rest...
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+        
+        Vector2 vel = body.getLinearVelocity();
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+        	vel.x -= 5;        	
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        	vel.x += 5;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+        	vel.y += 5;
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+        	vel.y -= 5;
+        } 
+        body.setLinearVelocity(vel);
+        m1.setPosition(body.getPosition().x, body.getPosition().y);
+
         batch.begin();
-        batch.draw(sprite, sprite.getX(), sprite.getY());
+        m1.draw(batch);
+        m2.draw(batch);
         batch.end();
+        
     }
 
     @Override
     public void dispose() {
-        // Hey, I actually did some clean up in a code sample!
-        img.dispose();
-        world.dispose();
+
     }
+
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
